956313b6694ea1002365a392cdd358f5b913723e
MaheryJeremie <mramahay2103@gmail.com>
2025-05-31 11:10:49 +0300

    First commit
    Initialisation de l'application

---
+++ evalRH/src/main/java/com/example/evalrh/EvalRhApplication.java
@@ -0,0 +1,13 @@
+package com.example.evalrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalRhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvalRhApplication.class, args);
+    }
+
+}
---
+++ evalRH/src/test/java/com/example/evalrh/EvalRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evalrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalRhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
